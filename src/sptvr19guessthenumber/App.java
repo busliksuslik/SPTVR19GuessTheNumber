9b300ee96b816ad5357dcddf0d691a30892017bd
Oleg <oleg.harlamov@ivkhk.ee>
2020-09-16 09:33:32 +0300

    Version 1.1 added user exit from game

--- src/sptvr19guessthenumber/App.java
+++ src/sptvr19guessthenumber/App.java
@@ -13,23 +13,32 @@ class App {
     private final Scanner scan = new Scanner(System.in);
     private final Random rand = new Random();
     public void run(){
-        int hidden = rand.nextInt(10000);
-        int concid = 0;
-        int numoftryes = 0;
-        System.out.printf("Number is guessed\n");
-        while(true){
-            System.out.println("write concidered number: ");
-            concid = scan.nextInt();
-            if (concid < hidden){
-                System.out.println("bigger");
-                numoftryes ++;
-            } else if (concid > hidden){
-                System.out.println("smaller");
-                numoftryes ++;
-            } else if (concid == hidden){
-                System.out.printf("Done in %d attempts %n", numoftryes);
+        while (true){
+            int hidden = rand.nextInt(10);
+            int concid = 0;
+            int numoftryes = 0;
+            String choise;
+            System.out.printf("Number is guessed\n");
+            while(true){
+                System.out.println("write concidered number: ");
+                concid = scan.nextInt();
+                if (concid < hidden){
+                    System.out.println("bigger");
+                    numoftryes ++;
+                } else if (concid > hidden){
+                    System.out.println("smaller");
+                    numoftryes ++;
+                } else if (concid == hidden){
+                    System.out.printf("Done in %d attempts %n", numoftryes);
+                    break;
+                }
+            }
+            System.out.println("Do you want to continue? Y/N");
+            scan.nextLine();
+            choise = scan.nextLine();
+            if (choise.equals("N")||choise.equals("n")){
                 break;
+            }
             }
         }
     }
-}
