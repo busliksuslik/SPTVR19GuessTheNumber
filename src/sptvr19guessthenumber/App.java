44cc511ceca4f14f95a875ce89876aa8c4a63f58
Oleg <oleg.harlamov@ivkhk.ee>
2020-09-16 09:17:08 +0300

    Version 1.0 stable

--- src/sptvr19guessthenumber/App.java
+++ src/sptvr19guessthenumber/App.java
@@ -5,10 +5,31 @@
  */
 package sptvr19guessthenumber;
 
-/**
- *
- * @author user
- */
+import java.util.Random;
+import java.util.Scanner;
+
+
 class App {
-    
+    private final Scanner scan = new Scanner(System.in);
+    private final Random rand = new Random();
+    public void run(){
+        int hidden = rand.nextInt(10000);
+        int concid = 0;
+        int numoftryes = 0;
+        System.out.printf("Number is guessed\n");
+        while(true){
+            System.out.println("write concidered number: ");
+            concid = scan.nextInt();
+            if (concid < hidden){
+                System.out.println("bigger");
+                numoftryes ++;
+            } else if (concid > hidden){
+                System.out.println("smaller");
+                numoftryes ++;
+            } else if (concid == hidden){
+                System.out.printf("Done in %d attempts %n", numoftryes);
+                break;
+            }
+        }
+    }
 }
